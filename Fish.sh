6a4b3a7e0ed5e2361e1e14bed822792f0d046389
Gmprado78 <gabrielprado109@gmail.com>
2026-03-03 15:07:57 -0500

    changed spome == signs based on the brackets they were using. realigning if statements. While loop broken atm.

--- Fish.sh
+++ Fish.sh
@@ -16,31 +16,41 @@ bad_counter=0
 while [ $my_health -ne 0 ]
 do
 	
+	# GAME START MESSAGES
+	#
+
 	#Display after every other turn
 	echo "Your health is $my_health"
 	echo "Wild fish health is $bad_health"
 	
 	#Fish wrecklessly attacking you
 	echo "OUCH the fish slapped you"
-	my_health -=10
+	let $my_health=$my_health-10
 	echo "You took 10hp, you have $my_health hp"
 
 	#Display attack to do
 	echo "punch fish?(Y)"
 	echo "don't punch fish.(N)"
 
+
+
 	#CHECK IF FISH IS ABOUT TO DIE BEFORE NEXT ATTACK	
 	#IF fish was hurt twice already(2hp)
 		
-	if [[ bad_health=2 ]];
+	if [[ $bad_health -le 2 ]];
 	then
 		echo "You can still befriend this fish."
 		echo "Give the fish a chance..???(Y/N)"
+
 		read answerz
+		
+		#Player chose to spare the fish
 		if [[ $answerz=='Y' ]]
 		then
-			echo"The fish of an unknown name swims off to the sunset alone. It doesn't even look back. It did not wish to befriend you."
+			echo "The fish of an unknown name swims off to the sunset alone. It doesn't even look back. It did not wish to befriend you."
 		fi
+
+		#Player chose to not spare the fish
 		if [[ $answerz=='N' ]]
 		then
 
@@ -50,6 +60,7 @@ do
 			echo "Game over."
 	
 			break
+		fi
 	
 	fi
 
@@ -57,41 +68,43 @@ do
 	
 	read answer
 
-	while [ $answer='Y' || $answer='N' ]
+	while [[ $answer=='Y' || $answer=='N' ]]
 	do
-		if [[ $answer='Y' ]]
+		if [[ $answer=='Y' ]]
 		then
-			bad_health -=49
+			let $bad_health=$bad_health-49
 		
 			echo "do you really wanna punch it? It just a fish."
 			echo "fish now has $bad_health hp"
+			return
 
-		#fish 4 turns to kill to me
 		#If I DONT punch fish counter goes up by one.
-		#human does 49 damage ( 3 punches to kill)
+		#Fisherman does 49 damage ( 3 punches to kill)
 
-		if [[ $answer='N' ]]; then
+		if [[ $answer=='N' ]]; then
 			echo "Fish flinches.(You didnt punch it)"
-		fi
+		
 		elif [ $answer='N' ] && [ $counter=1 ]; then
 			echo "(Fish raises eyebrow).You really don't wanna punch it.. you paci-fisht."
 		elif [ $answer='N' ] && [ $counter=2 ]; then
 			echo "Fish smiles and waves. It makes you pinky promise to never hurt a fish.EVER."
 			echo "do YOU accept this pinky promise? PUNCH or accept?(Y/N)"
 			read answer
+			
 
-		if [[ $answer='Y' ]]; then
-			echo "Fish says do you wanna hang out. You hold nands and swim together.The fish tells you its name, its Anita a name that means grace. You watch the sunset. Gameover.. did you win...?"
-			break
+			if [[ $answer=='Y' ]]; then
+				echo "Fish says do you wanna hang out. You hold nands and swim together.The fish tells you its name, its Anita a name that means grace. You watch the sunset. Gameover.. did you win...?"
+				break
+			fi
+
+			if [[ $answer=='N' ]]; then
+				echo "fish grabs your fist and throws you to the side. GAME OVER."
+				break
+			fi
+
+		else
+			echo "enter Y/N dude(plz restart script probs :] )"
 		fi
-
-		if [[ $answer='N' ]]; then
-			echo "fish grabs your fist and throws you to the side. GAME OVER."
-			break
-		fi
-
-	else
-		echo "enter Y/N dude(plz restart script probs :] )"
 	done
 	
 
