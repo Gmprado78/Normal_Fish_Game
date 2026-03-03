2b10c4fbea622b97c046e7fc235dee06ca8e53bc
Gmprado78 <gabrielprado109@gmail.com>
2026-03-02 20:03:15 -0500

    So i am making a satire fighting game. The player should identify multiple endings if clever enough. Currently added a few while loops and if statements along along with mostly just comments and print statements. Should not necessarily run at this point in the game.

---
+++ health.sh
@@ -0,0 +1,77 @@
+#! /bin/bash
+name=$1
+
+echo "You are fisherman named $name"
+echo "Knock the health out of that FISH!"
+
+my_health=100
+bad_health=100
+counter=0
+
+while [ $my_health -ne 0 ]
+do
+	
+	#Display after every other turn
+	echo "Your health is $my_health"
+	echo "Wild fish health is $bad_health"
+	
+
+	#Display attack to do
+	echo "punch fish?(Y)"
+	echo "don't punch fish.(N)"
+	read answer
+	while answer = Y or N:
+
+	if answer=Y
+	then
+		bad_health -=49
+		
+		echo "do you really wanna punch it? It just a fish."
+		echo "fish now has $bad_health hp"
+
+	if answer=N
+		echo "Fish flinches.(You didnt punch it)"
+	elif answer= N $$ counter=1
+		echo "(Fish raises eyebrow).You really don't wanna punch it.. you paci-fisht."
+	elif answer=N && counter=2
+		echo "	
+	else
+		echo "enter Y/N dude"
+	fi
+
+
+	if [ $bad_health=2 ]
+
+			
+
+	fi
+
+done
+
+#Display when attacked 
+echo "Ouch that fish Slapped me!"
+#Something that shows fish depleted health
+
+echo "health depleted __ points"
+
+echo "Do#Display after every other turn
+echo "Your health is $my_health"
+echo "Wild fish health is $bad_health"nt punch fish"
+
+## IF do not punch fish scenarios
+# Fish smiles and waves and makes you pinky promise to never hurt a fish. EVER.?
+# DO YOU ACCEPT? PUNCH OR accept?(Y/N)
+
+# if DO punch after yes, fish grabs your fist and throws you to the side. GAME OVER.
+# if you accept. Fish says do you wanna hang out. You hold nands and swim together.The fish tells you its name, its Anita a name means Grace. GAME OVER.
+
+
+##If do punch fish
+#fish 4 turns to kill to me
+#If I DONT punch fish counter goes up by one.
+#human does 49 damage ( 3 punches to kill)
+
+
+#Game over scene.
+#Did you win..??
+
